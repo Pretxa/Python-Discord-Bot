eeae9912467d78833bf7afde5c129232b69cb8ad
Pretza <69403099+Pretxa@users.noreply.github.com>
2021-09-18 20:18:40 +0200

    the bot

---
+++ main.py
@@ -0,0 +1,22 @@
+import discord
+client = discord.Client() #Brings the bot online somehow
+
+@client.event 
+async def on_message(message):
+
+    #Checks for 'hello' in any casing 
+    message.content = message.content.lower()
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("hello"):
+         await message.channel.send("Howdy hoe")
+
+    #Check for message author via Discord ID
+    if str(message.author) == "Skillz#6565":
+        await message.channel.send("Nice ass :)")
+    
+
+client.run("ODg4ODI4OTc5NTA5ODYyNTEx.YUYYfg.XUurxIkgpMMP5ToZ0uwKZe7sdMI")
+
+
